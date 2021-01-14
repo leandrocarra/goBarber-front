20b6b4fd203c5326070dbd7deec0a1f4b2e87055
leandrocarra <leandrocarra@gmail.com>
2021-01-14 14:19:26 -0300

    users

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -6,10 +6,13 @@ import * as Yup from 'yup';
 import logo from '~/assets/logo.svg';
 
 const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
   email: Yup.string()
     .email('Insira um e-mail válido')
     .required('O e-mail é obrigatório'),
-  password: Yup.string().required('A senha é obrigatória'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
 });
 
 export default  function SingUn() {
@@ -20,7 +23,7 @@ export default  function SingUn() {
     <>
       <img src={logo} alt="GoBarber" />
       <Form schema={schema} onSubmit={handleSubmit}>
-        <Input name="text" type="text" placeholder="Nome completo" />
+        <Input name="name" type="text" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input
           name="password"
